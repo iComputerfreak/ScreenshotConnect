d88a02adcfe8204d68c179a988f5b390c10b8980
Jonas Frey <dev@jonasfrey.de>
2023-06-01 22:51:38 +0200

    Initial Commit

---
+++ ScreenshotConnect/ScreenshotConnectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenshotConnectApp.swift
+//  ScreenshotConnect
+//
+//  Created by Jonas Frey on 01.06.23.
+//
+
+import SwiftUI
+
+@main
+struct ScreenshotConnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
